869f71253e97474a23c61a3f55a5f540cc1993f8
rabiul <rabiul17.ri@gmail.com>
2025-09-21 22:50:25 +0600

    Nothing

--- Python/habijabi.py
+++ Python/habijabi.py
@@ -7,7 +7,36 @@ print(a)
 print("your age is", 16)
 
 
+print("when a = 10")
+#name = input()
 
-name = input()
+#print("your name is:", name)
 
-print("your name is:", name)
+# Assuming 'a' is a previously defined variable. Let's assume a = 10 for these descriptions.
+
+print("10 // 5 is", 10 // 5)
+# This line performs integer division of 10 by 5. The result is the quotient without the remainder. The output will be 2.
+
+print("a / 5 is", a / 5)
+# This performs floating-point division of 'a' (which we assume is 10) by 5. The output will be 2.0.
+
+print("a % 3 is", a % 3)
+# This calculates the remainder when 'a' (10) is divided by 3. The output will be 1.
+
+print("a ** 3 is", a ** 3)
+# This calculates 'a' (10) raised to the power of 3. The output will be 1000.
+
+print("a + 3 is", a + 3)
+# This performs addition, adding 3 to 'a' (10). The output will be 13.
+
+print("a - 3 is", a - 3)
+# This performs subtraction, subtracting 3 from 'a' (10). The output will be 7.
+
+print("a * 3 is", a * 3)
+# This performs multiplication, multiplying 'a' (10) by 3. The output will be 30.
+
+a += 3
+print("After adding 3 to 'a', 'a' is", a)
+# This is a shorthand for 'a = a + 3'. It adds 3 to the current value of 'a' and updates the variable. The new value of 'a' will be 13.
+
+
---
+++ Python/string.py
@@ -0,0 +1,29 @@
+b = 'Neel'
+c = "Rabiul"
+d = "'''Hello World'''"
+e = '''"Hello World"'''
+
+print(b)
+print(c)
+print(d)
+print(e)
+
+
+print(b[2])
+
+#Formatted String
+name = "Rabiul"
+age = 16
+print(f"Your name is {name} and your age is {age}")
+
+
+print(f"Your name is {name} and your age is {input()} now")
+
+
+#python indentation
+a = 10
+
+if a == 10:
+    print("a is 10")
+   
+print("Nice")
